1eab402056d67f727e2b065723c5873496c8d80a
Joshua Stein <josh.t.stein@gmail.com>
2013-11-22 11:11:29 -0600

    Initial commit.

---
+++ Alistar/Models/DamageType.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alistar.Models
+{
+    public enum DamageType
+    {
+        PHYSICAL,
+        MAGICAL,
+        MIXED,
+        TRUE,
+        UNDEFINED
+    }
+}
---
+++ Alistar/Models/Rune.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alistar.Models
+{
+    public class Rune
+    {
+        public string Name { get; set; }
+        public string Magnitude { get; set; }
+        public RuneType RuneType { get; set; }
+        public string Descriptor { get; set; }
+        public Stat[] Stats { get; set; }
+    }
+}
